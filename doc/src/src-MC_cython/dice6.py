ba3498c5a902db16c74bd89be2bd846fa90059d8
Hans Petter Langtangen <hpl@simula.no>
2012-04-05 18:52:27 +0200

    updates

--- doc/src/src-MC_cython/dice6.py
+++ doc/src/src-MC_cython/dice6.py
@@ -4,7 +4,7 @@ import random
 
 def dice6_py(N, ndice, nsix):
     M = 0                     # no of successful events
-    for i in range(N):
+    for i in range(N):        # repeat N experiments
         six = 0               # how many dice with six eyes?
         for j in range(ndice):
             r = random.randint(1, 6)  # roll die no. j
--- doc/src/src-MC_cython/dice6.pyx
+++ doc/src/src-MC_cython/dice6.pyx
@@ -5,11 +5,10 @@ def dice6_cy1(int N, int ndice, int nsix):
     cdef int M = 0            # no of successful events
     cdef int six, r
     cdef double p
-    for i in range(N):
+    for i in range(N):        # repeat N experiments
         six = 0               # how many dice with six eyes?
         for j in range(ndice):
-            # Roll die no. j
-            r = random.randint(1, 6)
+            r = random.randint(1, 6)  # roll die no. j
             if r == 6:
                six += 1
         if six >= nsix:       # successful event?
